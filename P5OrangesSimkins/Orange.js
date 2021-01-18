ad9fa8fcf798d58b9bb1d81575eeeff40d0d6192
David Simkins <dwsimkins@gmail.com>
2021-01-18 15:50:57 -0500

    Added Oranges sample code

---
+++ P5OrangesSimkins/Orange.js
@@ -0,0 +1,13 @@
+Orange () {
+    constructor () {
+        this.r = (math.random()) * 30;
+        this.x = (math*random())*width;
+        this.y = (math*random())*height;
+    }
+    showCircle () {
+        circle (this.x,this.y,this.r);
+    }
+    moveCircle () {
+        
+    }
+}
---
+++ P5OrangesSimkins/sketch.js
@@ -0,0 +1,28 @@
+/**
+* Yourfirstand Lastname
+* IGME-102: Assignment Name, m/d/19
+* Summarization of sketch activity
+*/ 
+
+"use strict"; //catch some common coding errors
+
+/**
+* setup :
+*/
+let orange=[];
+
+function setup() {
+    for (let n=1, n<=10;n++) {
+        push orange(n)= new Orange;
+        console.log orange;
+    } 
+
+}
+
+/**
+* draw :
+*/
+function draw() {
+
+}
+ 
