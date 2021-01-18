c28530667409b652f39da34e2027a845021e32dd
David Simkins <dwsimkins@users.noreply.github.com>
2021-01-18 15:10:13 -0500

    Loading basic 102 files

---
+++ p5sketches/p5templateLastName/sketch.js
@@ -0,0 +1,22 @@
+/**
+* Yourfirstand Lastname
+* IGME-102: Assignment Name, m/d/19
+* Summarization of sketch activity
+*/ 
+
+"use strict"; //catch some common coding errors
+
+/**
+* setup :
+*/
+function setup() {
+
+}
+
+/**
+* draw :
+*/
+function draw() {
+
+}
+ 
